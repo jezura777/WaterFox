b9a4f8c337862ce67a1bdaed2dd703ea1afd1bfa
jezura777 <luki.fi@seznam.cz>
2023-06-11 16:40:30 +0200

    HistoryRepeats

--- start.sh
+++ start.sh
@@ -1,2 +1,13 @@
 java -Xmx3G -Xms3G -jar server.jar nogui
 # java -Xmx4G -Xms2G -XX:+UseG1GC -jar server.jar nogui
+
+DATE=$(date)
+
+echo "Commiting with timestamop: $DATE"
+
+git commit -m "$DATE"
+
+echo "Pushing to the branch Jezura777"
+
+git branch -M Jezura777
+git push
